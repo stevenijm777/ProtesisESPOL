2ea0ccc592839338d6e06acf8fb1d1b27e5b5c71
stevenijm <yosoystevenijm@gmail.com>
2024-06-29 23:03:51 -0500

    Modelo Convulucional

--- ProtesisHand/CodigoIA/model.py
+++ ProtesisHand/CodigoIA/model.py
@@ -1,9 +1,10 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import Sequential # type: ignore
-from tensorflow.keras.layers import Conv1D, MaxPooling1D, Flatten, Dense # type: ignore
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input, Conv1D, MaxPooling1D, Flatten, Dense
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.layers import Dropout, BatchNormalization
 import os
 
 # Procesamiento de Datos
@@ -12,7 +13,7 @@ import os
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Ruta a la subcarpeta que contiene los archivos de datos
-subfolder = 'HandLeftP2'
+subfolder = 'HandLeftP2'  # Cambiar aquí para trabajar con otra subcarpeta
 folder_path = os.path.join(script_dir, 'Datos', subfolder)
 
 # Función para cargar los datos desde un archivo de texto
@@ -59,3 +60,22 @@ X_test = scaler.transform(X_test.reshape(-1, 1)).reshape(-1, 1, 1)
 
 print("Datos procesados y listos para el entrenamiento del modelo.")
 
+# Construcción del modelo
+model = Sequential([
+    Input(shape=(1, 1)),
+    Conv1D(filters=32, kernel_size=1, activation='relu'),  # kernel_size ajustado a 1
+    MaxPooling1D(pool_size=1),
+    Flatten(),
+    Dense(100, activation='relu'),
+    Dense(5, activation='softmax')  # 5 movimientos diferentes
+])
+
+# Compilación del modelo
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Entrenamiento del modelo
+model.fit(X_train, y_train, epochs=10, batch_size=32, validation_split=0.2)
+
+# Evaluación del modelo
+loss, accuracy = model.evaluate(X_test, y_test)
+print(f'Loss: {loss}, Accuracy: {accuracy}')
